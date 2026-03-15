22ab8b96ddfa769ae024e440e989f3d5d3f1e80c
yonexdu0ra <phamngocquy.7303@gmail.com>
2026-03-15 14:08:44 +0700

    feat: cải thiện kết nối Redis bằng cách thêm publisher và subscriber

--- src/configs/redisClient.js
+++ src/configs/redisClient.js
@@ -1,18 +1,24 @@
-import {   createClient   } from "redis";
+import { createClient } from "redis";
 
 export const client = createClient({
     username: process.env.REDIS_USERNAME,
     password: process.env.REDIS_PASSWORD,
     socket: {
         host: process.env.REDIS_HOST,
-        port: process.env.REDIS_PORT
+        port: Number(process.env.REDIS_PORT),
     }
 });
+
+
+export const publisher = client.duplicate();
+export const subscriber = client.duplicate();
 
 client.on('error', err => console.log('Redis Client Error', err));
 const connectRedis = async () => {
     try {
         await client.connect();
+        await publisher.connect();
+        await subscriber.connect();
         console.log('connect to redis success !');
     } catch (error) {
         console.log('connect to redis failed !', error.message);
